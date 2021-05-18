5d33027ce6a7a66760400180d5a32eec06600dc8
Brian Workman <bworkman@frogdice.com>
2021-05-18 19:15:10 -0400

    added stripping emojis and also allowing bitly links

--- chat.js
+++ chat.js
@@ -7,9 +7,45 @@ const config = require(path.join(__dirname, "config"))
 const { Client, Message } = require("discord.js")
 const discordClient = new Client()
 
-/// Mudclient stuff
+// Mudclient stuff
 const net = require("net")
 const mudClient = new net.Socket()
+
+// Miscellaneous stuff
+const urlRegexSafe = require("url-regex-safe");
+const emojiRegexText = require("emoji-regex/text.js")
+const { write } = require("fs")
+const BitlyClient = require('bitly').BitlyClient;
+const { S_IFBLK } = require("constants")
+const bitly = new BitlyClient(config.bitlyToken);
+
+async function shortenUrls(message) {
+    let result
+    let newMessage = "" 
+
+    while( (result = urlRegexSafe().exec(message)) !== null ) {
+        const match = result[0] 
+        const index = result.index
+        let response
+        let shortenedUrl = null
+
+        newMessage += message.slice(0, index)
+        message = message.slice( (index + 1) + match.length -1 )
+
+        try {
+            response = await bitly.shorten(match) 
+            shortenedUrl = response.link
+        } catch (e) {
+            // Don't actually need to do anything, just silently ignore
+        } finally {
+            newMessage += shortenedUrl !== null ? shortenedUrl : match
+        }
+    }
+
+    newMessage += message 
+
+    return newMessage.toString()
+}
 
 function connectToMud() {
     mudClient.connect( config.mud_port, config.mud_ip, () => {
@@ -33,8 +69,6 @@ mudClient.on("data", data => {
     }     
 }) 
 
-connectToMud() ;
-
 discordClient.login(config.discordToken)
 .then( () => {
     console.log(`Logged into Discord`)
@@ -50,7 +84,7 @@ discordClient.login(config.discordToken)
     }
 })
 
-discordClient.on("message", message => {
+discordClient.on("message", async message => {
     // Do not accept zero-length content.
     if(message.content.length < 1) return ;
     if(message.member.user.bot === true) return ;
@@ -58,14 +92,35 @@ discordClient.on("message", message => {
     //Find the right channel to have listened on, discard any other channels
     for(channel of config.channels) {
         if(message.channel.id === channel.discord) {
-            const authorName = message.member.nickname || message.member.user.username
+            const mudChannel = channel.mud
+            let authorName = message.member.nickname || message.member.user.username
+            let text = message.content 
+
+            // If option to strip emoji is on, which is probably a good idea
+            // and is why it is the default
+            if( config.strip_emoji === true ) {
+                const regex = emojiRegexText()
+                authorName = authorName.replace(regex, "")
+                text = text.replace(regex, "") 
+            }
+
+            // After emoji stripping, need to now exit if our
+            // author or text is empty. 
+            if( authorName.length < 1 ) return ;
+            if( text.length < 1 ) return ;
+
+            // Now we need to parse urls if bitly is enabled
+            if(config.enable_bitly) text = await shortenUrls(text)
+
             const mudMessage = {
                 name: authorName,
-                channel: channel.mud,
-                message: message.content
+                channel: mudChannel,
+                message: text
             }
 
             mudClient.write(JSON.stringify(mudMessage))
         }
     }    
 })
+
+connectToMud() ;
--- config.js
+++ config.js
@@ -4,5 +4,6 @@ const path = require("path")
 
 const configObj = JSON.parse( fs.readFileSync(path.join(__dirname, "config.json") ) )
 configObj.discordToken = process.env.DISCORD_TOKEN
+configObj.bitlyToken = process.env.BITLY_TOKEN
 
 module.exports = configObj
